d60fd5b3118750a0f9fb3c850bd244db9c5a1a34
Hans Christian Schmitz <git@hcsch.eu>
2023-12-16 01:05:45 +0100

    vscode-extensions.mcu-debug.peripheral-viewer: init at 1.6.1

--- pkgs/applications/editors/vscode/extensions/default.nix
+++ pkgs/applications/editors/vscode/extensions/default.nix
@@ -3130,6 +3130,23 @@ let
         };
       };
 
+      mcu-debug.peripheral-viewer = buildVscodeMarketplaceExtension {
+        mktplcRef = {
+          name = "peripheral-viewer";
+          publisher = "mcu-debug";
+          version = "1.6.1";
+          sha256 = "sha256-DwaL0lct8KevC7AVFLydQTQEr1mC1Rz+P+jl+zHoN+k=";
+        };
+        meta = {
+          description = "Standalone Peripheral(SVD) Viewer extension extracted from cortex-debug, now works with any debugger";
+          downloadPage = "https://marketplace.visualstudio.com/items?itemName=mcu-debug.peripheral-viewer";
+          homepage = "https://github.com/mcu-debug/peripheral-viewer";
+          changelog = "https://marketplace.visualstudio.com/items/mcu-debug.peripheral-viewer/changelog";
+          license = lib.licenses.mit;
+          maintainers = [ lib.maintainers.hcsch ];
+        };
+      };
+
       mechatroner.rainbow-csv = buildVscodeMarketplaceExtension {
         mktplcRef = {
           name = "rainbow-csv";
