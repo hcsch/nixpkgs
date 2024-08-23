f19126223e54c22c22d268d788a45175b587967f
Jan Tojnar <jtojnar@gmail.com>
2024-08-23 08:38:18 +0200

    fixup! gegl: 0.4.48 → 0.4.48-unstable-2024-08-22

--- pkgs/development/libraries/gegl/default.nix
+++ pkgs/development/libraries/gegl/default.nix
@@ -123,7 +123,7 @@ stdenv.mkDerivation rec {
   '';
 
   # tests fail to connect to the com.apple.fonts daemon in sandboxed mode
-  doCheck = !stdenv.hostPlatform.isDarwin;
+  # doCheck = !stdenv.hostPlatform.isDarwin;
 
   passthru = {
     updateScript = unstableGitUpdater {
