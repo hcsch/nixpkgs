9c0776a40f1599b9f6e10381eb2662d185b1ef12
Hans Christian Schmitz <git@hcsch.eu>
2025-01-24 16:44:06 +0100

    coreutils: patch out gnulib physmem test
    physmem test fails in qemu for 32-bit systems where the host OS has a
    large amount of RAM total.

--- pkgs/tools/misc/coreutils/default.nix
+++ pkgs/tools/misc/coreutils/default.nix
@@ -122,6 +122,9 @@ stdenv.mkDerivation rec {
     + (optionalString stdenv.hostPlatform.isAarch64 ''
       # Sometimes fails: https://github.com/NixOS/nixpkgs/pull/143097#issuecomment-954462584
       sed '2i echo Skipping cut huge range test && exit 77' -i ./tests/cut/cut-huge-range.sh
+    '')
+    + (optionalString stdenv.hostPlatform.isMips32 ''
+      echo "int main() { return 77; }" > gnulib-tests/test-physmem.c
     '');
 
   outputs = [
